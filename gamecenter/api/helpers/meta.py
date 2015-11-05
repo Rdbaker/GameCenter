50e9e95257ed1b94afb1e0154ab386c3905efb79
Mike Perrone <mike.j.perrone@gmail.com>
2015-11-05 15:28:48 -0500

    get all the tests to pass

--- gamecenter/api/helpers/meta.py
+++ gamecenter/api/helpers/meta.py
@@ -7,9 +7,17 @@ def get_meta_from_args(count, args):
     """Construct the meta dictionary from the args"""
     meta = {}
     uri = current_app.config['HOST_URI']
+    offset = args['offset']
+    page_size = args['page_size']
     meta['total'] = count
-    meta['next'] = ((uri + "/api/leaderboards?offset={0}&page_size={1}")
-                    .format(int(args['offset']) + int(args['page_size']), int(args['page_size'])))
-    meta['prev'] = ((uri + "/api/leaderboards?offset={0}&page_size={1}")
-                    .format(int(args['offset']) - int(args['page_size']), int(args['page_size'])))
+    meta['links'] = {}
+
+    if offset + page_size < count:
+        meta['links']['next'] = (uri + "/api/leaderboards?offset={0}&page_size={1}"
+                                 .format(min(int(offset) + int(page_size), count),
+                                         int(page_size)))
+    if offset > 1:
+        meta['links']['prev'] = (uri + "/api/leaderboards?offset={0}&page_size={1}"
+                                 .format(max(int(offset) - int(page_size), 1),
+                                         int(page_size)))
     return meta
