16ee94e0d51df49950d5be87548a64600bcd22d0
Mike Perrone <mike.j.perrone@gmail.com>
2015-12-08 13:05:19 -0500

    have the db deploy use an env var to find the db password

--- deploy/conf.sh
+++ deploy/conf.sh
@@ -2,4 +2,7 @@ export PGHOST=rankdb.coomuod9pyh1.us-west-2.rds.amazonaws.com
 export PGPORT=5432
 export PGDATABASE=rankdb
 export PGUSER=rankuser
+
 export PGPASSWORD=rankpassword
+
+export TF_VAR_pgpass=$PGPASSWORD
