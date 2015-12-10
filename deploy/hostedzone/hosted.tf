9e2e45f423dfe17c4a5ec82653b9bd85b1c493bd
Mike Perrone <mike.j.perrone@gmail.com>
2015-12-10 10:33:05 -0500

    make output format prettier

--- deploy/hostedzone/hosted.tf
+++ deploy/hostedzone/hosted.tf
@@ -11,5 +11,5 @@ output "hosted_zone_id" {
 }
 
 output "name_servers" {
-    value = "${aws_route53_zone.tmwild.name_servers}"
+    value = "${aws_route53_zone.tmwild.name_servers.0},${aws_route53_zone.tmwild.name_servers.1},${aws_route53_zone.tmwild.name_servers.2},${aws_route53_zone.tmwild.name_servers.3}"
 }
