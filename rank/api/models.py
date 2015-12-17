bcc1e98d1636354fd05a4b265d3c357da675c293
Ryan Baker <ryan.da.baker@gmail.com>
2015-12-16 19:35:47 -0500

    Fixes a small bug in the scores created_at field.

--- rank/api/models.py
+++ rank/api/models.py
@@ -20,7 +20,7 @@ class Score(Base):
     user_id = db.Column(db.Integer, index=True, nullable=False)
     score = db.Column(db.Integer, nullable=False)
     tag = db.Column(db.String, index=True)
-    created_at = db.Column(db.DateTime, nullable=False, default=arrow.utcnow().datetime)
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda x: arrow.utcnow().datetime)
 
     game_id = db.Column(db.Integer, db.ForeignKey("games.id"))
     game = relationship("Game", backref=backref("scores"), cascade="delete-orphan, delete", single_parent=True)
